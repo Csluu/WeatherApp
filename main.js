fed202f83f3fecdfa148f903a3cb6d36b5519ccc
Chris <csluu@protonmail.com>
2023-11-25 20:45:36 -0500

    Second instance logic
    Will close the application if there is another instance and will focus the current instance instead.

--- main.js
+++ main.js
@@ -64,21 +64,41 @@ const createMainWindow = () => {
 	// Get the button element
 };
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-	createMainWindow();
+// * App Initialization
+// Request single instance lock
+// Making sure if there is another instance of this application or not.
 
-	// Remove mainWindow from memory on close to prevent memory leak
-	mainWindow.on("closed", () => (mainWindow = null));
+const gotTheLock = app.requestSingleInstanceLock();
 
-	app.on("activate", () => {
-		if (BrowserWindow.getAllWindows().length === 0) {
-			createMainWindow();
+if (!gotTheLock) {
+	// If there is another instance running quit this new instance
+	console.log("Application is already running");
+	app.quit();
+} else {
+	app.on("second-instance", (event, commandLine, workingDirectory) => {
+		// Someone tried to run a second instance, we should focus our window
+		if (mainWindow) {
+			if (mainWindow.isMinimized()) mainWindow.restore();
+			mainWindow.focus();
 		}
 	});
-});
+
+	// This method will be called when Electron has finished
+	// initialization and is ready to create browser windows.
+	// Some APIs can only be used after this event occurs.
+	app.whenReady().then(() => {
+		createMainWindow();
+
+		// Remove mainWindow from memory on close to prevent memory leak
+		mainWindow.on("closed", () => (mainWindow = null));
+
+		app.on("activate", () => {
+			if (BrowserWindow.getAllWindows().length === 0) {
+				createMainWindow();
+			}
+		});
+	});
+}
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
